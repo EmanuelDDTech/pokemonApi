9d515601522c6657852e679e49e91a97f9c48e11
EmanuelDDTech <emanuel@ddtech.mx>
2024-08-19 18:02:47 -0600

    Pokemon Api project

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
+
+const { gameStatus, isLoading, randomPokemon } = usePokemonGame();
+</script>
+
+<template>
+  <section
+    v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animate-pulse">Cargando Pokémons</h3>
+  </section>
+
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Quién es este Pokémon</h1>
+    <h3>{{ randomPokemon }}</h3>
+
+    <!-- Pokemon Picture -->
+    <PokemonPicture
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
+
+    <!-- Pokemon Options -->
+    <PokemonOptions />
+  </section>
+</template>
